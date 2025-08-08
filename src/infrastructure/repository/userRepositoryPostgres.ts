e84bce9fcbe961e48aa7c3d1e3a98af54c7dcde6
GibsonCS <gibson.cruzds@gmail.com>
2025-08-08 13:19:19 -0300

    feat(infra) change return type from userRepositoryPostgres to userDTO

--- src/infrastructure/repository/userRepositoryPostgres.ts
+++ src/infrastructure/repository/userRepositoryPostgres.ts
@@ -1,8 +1,11 @@
 import type User from "../../core/entities/user";
 import type { UserRepository } from "../../core/repositories/userRepository";
+import type { UserDTO } from "../../core/use-cases/create-user/userDTO";
 
+const users: UserDTO[] = [];
 export default class UserRepositoryPostgres implements UserRepository {
-  async save(item: User): Promise<User | undefined> {
-    return item;
+  async save(item: UserDTO): Promise<UserDTO | undefined> {
+    users.push(item);
+    return users[0];
   }
 }
