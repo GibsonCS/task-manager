dbe180628d7aa0e08ad476042ce83529188e32a0
GibsonCS <gibson.cruzds@gmail.com>
2025-08-08 13:17:25 -0300

    feat(core) add create user method in user entity

--- src/core/entities/user.ts
+++ src/core/entities/user.ts
@@ -1,11 +1,46 @@
-export default class User {
-  private name: string;
-  private username: string;
-  private password: string;
+import InvalidUser from "../../errs/invalidUser";
+import type { UserDTO } from "../use-cases/create-user/userDTO";
 
-  constructor(name: string, username: string, password: string) {
-    this.name = name;
-    this.username = username;
-    this.password = password;
+export default class User {
+  private _name: string;
+  private _username: string;
+  private _password: string;
+
+  private constructor(name: string, username: string, password: string) {
+    this._name = name;
+    this._username = username;
+    this._password = password;
+  }
+
+  public static create({ name, username, password }: UserDTO): User {
+    if (!this.isValidUsername(username)) {
+      throw new InvalidUser("username must be five or more characters");
+    }
+
+    if (!this.isValidName(name)) {
+      throw new InvalidUser("name must be five or more characters");
+    }
+
+    return new User(name, username, password);
+  }
+
+  private static isValidUsername(username: string): Boolean {
+    return username.length >= 5;
+  }
+
+  private static isValidName(name: string) {
+    return name.length >= 5;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get username(): string {
+    return this._username;
+  }
+
+  get password(): string {
+    return this._password;
   }
 }
