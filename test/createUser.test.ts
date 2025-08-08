528bc6a3d2e3f840c419320a096fe8d4baa15c0a
GibsonCS <gibson.cruzds@gmail.com>
2025-08-08 13:15:12 -0300

    test(createUser): add test to create user usecase

--- test/createUser.test.ts
+++ test/createUser.test.ts
@@ -1,15 +1,56 @@
 import { describe, test, expect } from "@jest/globals";
-import User from "../src/core/entities/user";
-import CreateUser from "../src/core/use-cases/createUser";
 import UserRepositoryPostgres from "../src/infrastructure/repository/userRepositoryPostgres";
+import CreateUser from "../src/core/use-cases/create-user/createUser";
+import type { UserDTO } from "../src/core/use-cases/create-user/userDTO";
+import InvalidUser from "../src/errs/invalidUser";
 
 describe("createUser", () => {
-  test("should return a created user", async () => {
-    const user = new User("Gibson", "admin", "123");
-
+  test("should throw a InvalidUser with 'username must be five or more characters' message", async () => {
     const createUser = new CreateUser(new UserRepositoryPostgres());
-    const result = await createUser.execute(user);
 
-    expect(result).toStrictEqual(user);
+    const input: UserDTO = {
+      name: "gibson",
+      username: "gb",
+      password: "123456789",
+    };
+
+    await expect(createUser.execute(input)).rejects.toThrow(
+      "username must be five or more characters"
+    );
+  });
+
+  test("should throw a InvalidUser with the message 'name must be five or more characters'", async () => {
+    const createUser = new CreateUser(new UserRepositoryPostgres());
+
+    const input: UserDTO = {
+      name: "b",
+      username: "gibson.cruz",
+      password: "123456789",
+    };
+
+    //"rejects.toThrow" used to async functions
+    await expect(createUser.execute(input)).rejects.toThrow(
+      "name must be five or more characters"
+    );
+  });
+
+  test("should return a valid created user", async () => {
+    const createdUser = new CreateUser(new UserRepositoryPostgres());
+
+    const input: UserDTO = {
+      name: "Gibson Cruz",
+      username: "gibso.cruz",
+      password: "1234568",
+    };
+
+    const output: UserDTO = {
+      name: "Gibson Cruz",
+      username: "gibso.cruz",
+      password: "1234568",
+    };
+
+    const result = await createdUser.execute(input);
+
+    expect(result).toStrictEqual(output);
   });
 });
