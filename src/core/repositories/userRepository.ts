1610480b3c358b09a34388d6400f993b3fa0f00c
GibsonCS <gibson.cruzds@gmail.com>
2025-08-08 13:16:41 -0300

    feat(core) change return type from userRepository to userDTO

--- src/core/repositories/userRepository.ts
+++ src/core/repositories/userRepository.ts
@@ -1,4 +1,5 @@
 import type User from "../entities/user";
+import type { UserDTO } from "../use-cases/create-user/userDTO";
 import type { IBaseRepository } from "./baseRepository";
 
-export interface UserRepository extends IBaseRepository<User> {}
+export interface UserRepository extends IBaseRepository<UserDTO> {}
